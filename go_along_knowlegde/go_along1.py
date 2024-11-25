719338da2e7704042fe6f2182deccef1b4b666b5
Abhilashg11 <alexrog1122@gmail.com>
2024-11-25 15:59:34 +0530

    logger

--- go_along_knowlegde/go_along1.py
+++ go_along_knowlegde/go_along1.py
@@ -23,3 +23,27 @@ print(f.read(200))
 print(f.readline(200))
 
 ''' 200 is byte ''' 
+
+
+#compile or parser and runtime
+
+'''compile'''
+# if True
+#     print("hello")
+
+'''runtime'''
+# print(10/0)
+
+#assignment
+x,y,z = 10,20,30
+print(x,y,z)
+
+## __str__
+
+class MyClass:
+    # pass
+    def __str__(self):
+        return "this is myclass"
+
+obj = MyClass()
+print(obj)
--- program_challenges/practise1.py
+++ program_challenges/practise1.py
@@ -1,3 +1,31 @@
-n = int(input("enter the values").strip())
-for i in range(n):
-    print(i*i)
+# # # n = int(input("enter the values").strip())
+# # li = [1,2,3,4]
+# # # for i in range(1, n+1):
+# # #     li.append(i)
+
+# # # print(li)
+# # text =""
+# # for i in li:
+# #     text = text+str(i)
+# # print(text)
+
+
+
+# s = set('HackerRank')
+# s.add('H')
+# print(s)
+
+# # set(['a', 'c', 'e', 'H', 'k', 'n', 'r', 'R'])
+# print(s.add('HackerRank'))
+
+# print( s)
+# # set(['a', 'c', 'e', 'HackerRank', 'H', 'k', 'n', 'r', 'R'])
+
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
+    
+l = [[x1,y1,z1] for x1 in range(x+1) for y1 in range(y+1) for z1 in range(z+1) if(x1+y1+z1)!=n]
+    
+print(l)
