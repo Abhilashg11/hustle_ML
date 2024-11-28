012083745c58b6c996ac2119d92b43ce24939b45
Abhilashg11 <alexrog1122@gmail.com>
2024-11-28 18:54:33 +0530

    data_ingestion

--- Ml_Project/src/components/data_transformation.py
+++ Ml_Project/src/components/data_transformation.py
@@ -1,1 +1,26 @@
-  
+import sys
+from dataclasses import dataclass
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
+
+from scr.execption import CustomExecption
+from src.logger import logging
+import os
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path = os.path.join('artifacts')
+
+class DataTransformation:
+    def __init__:
+        self.data_transformation_config = DataTransformationConfig()
+
+    def get_data_transformer_object(self):
+        try:
+            pass
+        except:
+            pass
