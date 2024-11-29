95216803a9e5c63a5676c6a3d4730a37a9e414ee
Abhilashg11 <alexrog1122@gmail.com>
2024-11-29 13:34:25 +0530

    decorator

--- go_along_knowlegde/go_along1.py
+++ go_along_knowlegde/go_along1.py
@@ -47,3 +47,30 @@ class MyClass:
 
 obj = MyClass()
 print(obj)
+
+
+##class decorator
+
+import time
+
+class TimerDecorator:
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        start_time = time.time()
+        result = self.func(*args, **kwargs)
+        end_time = time.time()
+        print(f"Function {self.func.__name__} executed in {end_time - start_time} seconds")
+        return result
+
+@TimerDecorator
+def example_function(n,m):
+    total = 0
+    for i in range(n):
+        total += i
+    print(m)
+    return total
+
+# Usage
+print(example_function(1000000,"time"))
--- program_challenges/practise1.py
+++ program_challenges/practise1.py
@@ -21,13 +21,65 @@
 # print( s)
 # # set(['a', 'c', 'e', 'HackerRank', 'H', 'k', 'n', 'r', 'R'])
 
-x = int(input())
-y = int(input())
-z = int(input())
-n = int(input())
+# x = int(input())
+# y = int(input())
+# z = int(input())
+# n = int(input())
     
-l = [[x1,y1,z1] for x1 in range(x+1) for y1 in range(y+1) for z1 in range(z+1) if(x1+y1+z1)!=n]
+# l = [[x1,y1,z1] for x1 in range(x+1) for y1 in range(y+1) for z1 in range(z+1) if(x1+y1+z1)!=n]
     
-print(l)
+# print(l)
 
-print(dtype(0))
+# print(dtype(0))
+
+# def shout(text):
+#     return text.upper()
+# yell = shout
+
+# def ell(fun):
+#     greet = fun("hi how are ")
+#     print(greet)
+
+# ell(shout)
+
+
+
+# import time
+
+# class TimerDecorator:
+#     def __init__(self, func):
+#         self.func = func
+
+#     def __call__(self, *args, **kwargs):
+#         start_time = time.time()
+#         result = self.func(*args, **kwargs)
+#         end_time = time.time()
+#         print(f"Function {self.func.__name__} executed in {end_time - start_time} seconds")
+#         return result
+
+# @TimerDecorator
+# def example_function(n,m):
+#     total = 0
+#     for i in range(n):
+#         total += i
+#     print(m)
+#     return total
+
+# # Usage
+# print(example_function(1000000,"time"))
+
+class Data:
+    def __init__(self,a,b,c):
+        self.a = a
+        self.b = b
+        self.c = c
+        
+    
+    def compute(self,a,b,c):
+        r = self.a+self.b+self.c
+        return r
+
+
+obj1= Data(2,2,2)
+res = obj1.compute()
+print(res)
